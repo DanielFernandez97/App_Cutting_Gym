908c72222c46bb0d1915fa178f399660f33d1062
DANFEROBI\danie <daniel.fdez1711@gmail.com>
2024-12-02 16:59:42 +0100

    [UPLOAD] Debbuging

--- Main_application/AppDefi_SQL.py
+++ Main_application/AppDefi_SQL.py
@@ -2,41 +2,42 @@ import datetime as dt
 import sqlite3 as sql
 import pandas as pd
 
-"""Igual a la hora de crear la tabla control_pesos habria 
+"""Igual a la hora de crear la tabla weight_control habria 
 que cambiar el formato de fecha y ponerle TEXT, 
 TIMESTAMP es un sello de fecha que va desde 1970, milisegundos desde
 """
+
 def creacion_database():
-    conexion = sql.connect("Tabla_Definicion.db")
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
     conexion.commit()
     conexion.close()
 
 def creacion_tabla():
-    conexion = sql.connect("Tabla_Definicion.db")
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
-    cursor.execute("""CREATE TABLE control_pesos(
-                   Fecha TIMESTAMP,
-                   Peso  DOUBLE,
-                   Peso_Dia_anterior DOUBLE,
-                   Peso_Promedio_Semana_Anterior DOUBLE,
-                   Peso_VS_DiaAnterior DOUBLE,
-                   Peso_VS_SemanaAnterior DOUBLE,
-                   IMC DOUBLE,
-                   Calorias_Entreno INT,
+    cursor.execute("""CREATE TABLE weight_control(
+                   Date TIMESTAMP,
+                   Weight  DOUBLE,
+                   Weight_Date_Before DOUBLE,
+                   Average_Weight_Week_Before DOUBLE,
+                   Weight_vs_DayBefore DOUBLE,
+                   Weight_vs_WeekBefore DOUBLE,
+                   BMI DOUBLE,
+                   Training_Calories INT,
                    Cardio_TF BOOLEAN,
-                   Calorias_Cardio INT,
-                   Calorias_totales INT)
+                   Cardio_Calories INT,
+                   Total_Calories INT)
     """)
     conexion.commit()
     conexion.close()
 
 def insertar_fila(fecha,peso, estatura, tiempo_entreno, cardio, tiempo_cardio,edad):
-    conexion = sql.connect("Tabla_Definicion.db")
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
     MET_gym = 6
     MET_cardio = 4.5
-    instruccion = f"""INSERT INTO control_pesos VALUES (
+    instruccion = f"""INSERT INTO weight_control VALUES (
     '{fecha}', {peso},{peso-0.7},{round(0,2)},{round(peso-(peso+0.7),2)},{round(0,2)},{peso/(estatura/100)},
     {((round(MET_gym*3.5*peso)/200)*tiempo_entreno,2)},{cardio},{round(tiempo_cardio*((MET_cardio*3.5*peso)/200),2)},
     {((round((MET_gym*3.5*peso)/200)*tiempo_entreno)+(tiempo_cardio*((MET_cardio*3.5*peso)/200))+((10*peso)+(6.25*estatura)-(5*(edad)+5)),2)}
@@ -46,24 +47,24 @@ def insertar_fila(fecha,peso, estatura, tiempo_entreno, cardio, tiempo_cardio,ed
     conexion.close()
 
 def insertar_variasfilas(añadir_varios_datos):
-    conexion = sql.connect("Tabla_Definicion.db")
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
-    cursor.executemany('INSERT INTO control_pesos VALUES (?,?,?,?,?,?,?,?,?,?,?)', añadir_varios_datos)
+    cursor.executemany('INSERT INTO weight_control VALUES (?,?,?,?,?,?,?,?,?,?,?)', añadir_varios_datos)
     conexion.commit()
     conexion.close()
 
 def borrar_fila(condicion_fecha):
-    conexion = sql.connect('Tabla_Definicion.db')
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
-    instruccion = f'DELETE FROM control_pesos WHERE Fecha = "{condicion_fecha}"'
+    instruccion = f'DELETE FROM weight_control WHERE Date = "{condicion_fecha}"'
     cursor.execute(instruccion)
     conexion.commit()
     conexion.close()
 
 def lectura_filas():
-    conexion = sql.connect('Tabla_Definicion.db')
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
-    instruccion = 'SELECT * FROM control_pesos ORDER BY Fecha DESC'
+    instruccion = 'SELECT * FROM weight_control ORDER BY Date DESC'
     cursor.execute(instruccion)
     datos = cursor.fetchall()
     conexion.commit()
@@ -71,35 +72,35 @@ def lectura_filas():
     return datos
 
 def filtrado_tabla(): 
-    conexion = sql.connect('Tabla_Definicion.db')
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
     tipo_filtrado = input('El tipo de filtrado que quieres realizar es filas o columnas o ambos? (escribe: "f" o "c"): ')
     if tipo_filtrado == 'c':
         columna1 = input('Introduce la primera columna: ')
         columna2 = input('Introduce la segunda columna: ')
-        instruccion = f'SELECT {columna1}, {columna2} FROM control_pesos ORDER BY Fecha DESC'
+        instruccion = f'SELECT {columna1}, {columna2} FROM weight_control ORDER BY Date DESC'
 
     if tipo_filtrado == "f":
         eleccion = int(input('Escoge un tipo de filtrado:\n 1-Filtrado "="\n 2-Filtrado Between\n 3-Filtrado "> o <"\n    -->'))
         if eleccion == 1:
             formato_ejemplo = dt.datetime.now().strftime('%Y-%m-%d')
             fecha_formato = input(f'Cual es la fecha que quieres introducir (ejemplo:{formato_ejemplo}): ')
-            instruccion = f'SELECT * FROM control_pesos WHERE Fecha = "{fecha_formato}"'
+            instruccion = f'SELECT * FROM weight_control WHERE Date = "{fecha_formato}"'
         if eleccion == 2:
             formato_ejemplo = dt.datetime.now().strftime('%Y-%m-%d')
             fecha_inicial = input(f'Inserta la fecha INICIAL (ejemplo: {formato_ejemplo}): ')
             fecha_final = input(f'Inserta la fecha Final (ejemplo: {formato_ejemplo}): ')
-            instruccion = f'SELECT * FROM control_pesos WHERE Fecha BETWEEN "{fecha_inicial}" AND "{fecha_final}" ORDER BY Fecha DESC'
+            instruccion = f'SELECT * FROM weight_control WHERE Date BETWEEN "{fecha_inicial}" AND "{fecha_final}" ORDER BY Date DESC'
         if eleccion == 3:
             operacion = input('Filtrar por "mayor" o "menor": ').lower()
             if operacion == "mayor":
                 formato_ejemplo = dt.datetime.now().strftime('%Y-%m-%d')
                 fecha_formato = input(f'Cual es la fecha que quieres introducir (ejemplo: {formato_ejemplo}): ')
-                instruccion = f'SELECT * FROM control_pesos WHERE Fecha > "{fecha_formato}" ORDER BY Fecha DESC'
+                instruccion = f'SELECT * FROM weight_control WHERE Date > "{fecha_formato}" ORDER BY Date DESC'
             if operacion == "menor":
                 formato_ejemplo = dt.datetime.now().strftime('%Y-%m-%d')
                 fecha_formato = input(f'Cual es la fecha que quieres introducir (ejemplo: {formato_ejemplo}): ')
-                instruccion = f'SELECT * FROM control_pesos WHERE Fecha < "{fecha_formato}" ORDER BY Fecha DESC'
+                instruccion = f'SELECT * FROM weight_control WHERE Date < "{fecha_formato}" ORDER BY Date DESC'
     cursor.execute(instruccion)
     datos = cursor.fetchall()
     conexion.commit()
@@ -107,9 +108,9 @@ def filtrado_tabla():
     print(datos)
 
 def nombre_columnas():
-    conexion = sql.connect('Tabla_Definicion.db')
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
-    instruccion = 'SELECT name FROM pragma_table_info("control_pesos")'
+    instruccion = 'SELECT name FROM pragma_table_info("weight_control")'
     cursor.execute(instruccion)
     columnas = cursor.fetchall()
     conexion.commit()
@@ -117,7 +118,7 @@ def nombre_columnas():
     return columnas
 
 def lanza_querys(query):
-    conexion = sql.connect('Tabla_Definicion.db')
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
     instruccion = query
     cursor.execute(instruccion)
@@ -127,9 +128,9 @@ def lanza_querys(query):
     return datos
 
 def auxilia_consultas(columna, fecha):
-    conexion = sql.connect('Tabla_Definicion.db')
+    conexion = sql.connect('Cutting_table.db')
     cursor = conexion.cursor()
-    instruccion = f'SELECT {columna} FROM control_pesos WHERE Fecha = "{fecha}"'
+    instruccion = f'SELECT {columna} FROM weight_control WHERE Date = "{fecha}"'
     cursor.execute(instruccion)
     peso = cursor.fetchall()
     conexion.commit()
--- Main_application/App_ventana.py
+++ Main_application/App_ventana.py
@@ -2,7 +2,20 @@ from tkinter import ttk
 from tkinter import *
 from tkinter import Text
 
-from chatbot_gym import *
+import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+INITIAL_PATH = os.getenv("INITIAL_PATH")
+REPO_GYM_CUT = os.getenv("FOLDER_NAME")
+
+ruta_proyecto = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(str(INITIAL_PATH), str(REPO_GYM_CUT)))
+
+
+from Chatbot_model_from_scratch.chatbot_gym import *
+from Chatbot_model_pretrained.Chatbot_pretrained import *
 
 import os
 
@@ -15,33 +28,60 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
+#TODO
+# Pasar variables al ingles
 
 
-TABLA = 'Tabla_Definicion.db'
+#Futuros desarrollos
+#Incluir fotografia y comentario con IA sobre la evolucion del fisico, se necesitaran labelframe mas, necesitariamos dataset con los label
+#Incluir un chatbot de un modelo desde 0 donde el usuario pueda preguntar cosas sobre el gym puede ser modelo fine tuned con info gym
+
+
+TABLE = 'Cutting_table.db'
 DIALOGUE_LIST = []
+
+def add_placeholder(entry, placeholder):
+    # Añadir el texto por defecto al Entry
+    entry.insert(0, placeholder)
+    entry.config(fg="#D3D3D3")  # Estilo visual para diferenciar el placeholder
+
+    # Función que elimina el placeholder cuando el usuario hace clic
+    def on_focus_in():
+        if entry.get() == placeholder:
+            entry.delete(0, END)
+            entry.config(fg="black")  # Cambia el color al escribir texto
+
+    # Función que restaura el placeholder si el usuario deja el campo vacío
+    def on_focus_out():
+        if entry.get() == "":
+            entry.insert(0, placeholder)
+            entry.config(fg="#D3D3D3")
+
+    # Asignar los eventos
+    entry.bind("<FocusIn>", on_focus_in)
+    entry.bind("<FocusOut>", on_focus_out)
 
 class Pesos:
 
     def __init__(self,root):
 
+        self.estatura = int(170)
+        self.MET_gym = float(6)
+        self.MET_cardio = float(3.5)
+
         self.wind = root
-        self.wind.title('App Definicion 2024')
+        self.wind.title('Gym Cutting App')
         self.wind.geometry("1750x1150")
 
-        frame1 = LabelFrame(self.wind, text='Datos Pesos por fecha', font=("Helvetica", 14)) 
+        frame1 = LabelFrame(self.wind, text='Weight by Date data', font=("Helvetica", 14)) 
 
-        frame2 = LabelFrame(self.wind, text='Filtrado para los pesos', font=("Helvetica", 14))
+        frame2 = LabelFrame(self.wind, text='Weight filtered', font=("Helvetica", 14))
 
-        frame3 = LabelFrame(self.wind, text='Graficado', font=("Helvetica", 14))
+        frame3 = LabelFrame(self.wind, text='Graphics', font=("Helvetica", 14))
         self.frame3 = frame3
 
-        frame4 = LabelFrame(self.wind, text='Preguntas Chatbot', font=("Helvetica", 14))
+        frame4 = LabelFrame(self.wind, text='Gym Chatbot', font=("Helvetica", 14))
 
-
-        #Voy a tener que cambiar la estructura y no hacerlo con pack sino con grid() o place() que es mucho mas versatil
-        #frame1.pack(fill='both', expand='no',padx=30,pady=20, side=TOP)
-        #frame2.pack(fill='x', expand='no', side=LEFT)
-        #frame3.pack(fill='both', expand='yes',padx=20,pady=10,side=BOTTOM)
         frame1.place(x=100,y=10, width=1600)
         frame2.place(x=100, y=280, width=750)
         frame3.place(x=900, y=280, width=800, height=793)
@@ -50,7 +90,6 @@ class Pesos:
         self.trv = ttk.Treeview(frame1, columns= (1,2,3,4,5,6,7,8,9,10,11), show='headings', height=10)
         self.trv.grid()
 
-        #Importante, hay que intentar que al tirar una query solo muestre las columnas de la query
         columnas = app.nombre_columnas()
         for numero, encabezado in enumerate(columnas):
             self.trv.heading(numero+1,text=list(encabezado))
@@ -61,31 +100,34 @@ class Pesos:
             self.trv.column(numero+1, width=anchura)
         self.consulta()
 
-        self.text = Text(frame4, background="white", width=140, height=25)
+        self.text = Text(frame4, background="white", width=90, height=25)
         self.text.config(state="normal")
         self.text.grid()
 
 
-        lbl1 = Label(frame2, text='Fecha del peso', width= 20)
+        lbl1 = Label(frame2, text='Weighing Date', width= 20)
         lbl1.grid(row=0, padx=8, pady=5)
         self.ent1 = Entry(frame2)
         self.ent1.grid(row=0, column=1, padx=8, pady=5)
+        add_placeholder(entry=self.ent1, placeholder="XX/MM/YYYY")
 
         lbl2 = Label(frame2, width= 20)
         lbl2.grid(row=1, padx=8, pady=5)
 
-        lbl3 = Label(frame2, text='Peso', width= 20)
+        lbl3 = Label(frame2, text='Weight', width= 20)
         lbl3.grid(row=1, padx=8, pady=5)
         self.ent2 = Entry(frame2)
         self.ent2.grid(row=1, column=1, padx=8, pady=5)
+        add_placeholder(entry=self.ent2, placeholder="xx.xx")
 
         lbl4 = Label(frame2, width= 20)
         lbl4.grid(row=2, padx=8, pady=5)
 
-        lbl5 = Label(frame2, text='Tiempo entreno', width= 20)
+        lbl5 = Label(frame2, text='Training Time', width= 20)
         lbl5.grid(row=2, padx=8, pady=5)
         self.ent3 = Entry(frame2)
         self.ent3.grid(row=2, column=1, padx=8, pady=5)
+        add_placeholder(entry=self.ent3, placeholder="xx.xx")
 
         lbl6 = Label(frame2, width= 20)
         lbl6.grid(row=3, padx=8, pady=5)
@@ -94,53 +136,58 @@ class Pesos:
         lbl7.grid(row=3, padx=8, pady=5)
         self.ent4 = Entry(frame2)
         self.ent4.grid(row=3, column=1, padx=8, pady=5)
+        add_placeholder(entry=self.ent4, placeholder="0/1 values only")
 
         lbl8 = Label(frame2, width= 20)
         lbl8.grid(row=4, padx=8, pady=5)
 
-        lbl9 = Label(frame2, text='Tiempo Cardio', width= 20)
+        lbl9 = Label(frame2, text='Cardio Time', width= 20)
         lbl9.grid(row=4, padx=8, pady=5)
         self.ent5 = Entry(frame2)
         self.ent5.grid(row=4, column=1, padx=8, pady=5)
+        add_placeholder(entry=self.ent5, placeholder="xx.xx")
 
         lbl10 = Label(frame2, width= 20)
         lbl10.grid(row=5, padx=8, pady=5)
 
-        lbl11 = Label(frame2, text='Edad', width= 20)
+        lbl11 = Label(frame2, text='Age', width= 20)
         lbl11.grid(row=5, padx=8, pady=5)
         self.ent6 = Entry(frame2)
         self.ent6.grid(row=5, column=1, padx=8, pady=5)
+        add_placeholder(entry=self.ent6, placeholder="Only integers")
 
         lbl12 = Label(frame2, width= 20)
         lbl12.grid(row=6, padx=8, pady=5)
 
-        btt1 = Button(frame2, text='Agregar',command=self.agregar, width=12, height=2)
+        btt1 = Button(frame2, text='Add',command=self.agregar, width=12, height=2)
         btt1.grid(row=0, column=4)
 
-        btt2 = Button(frame2, text='Filtrar', command=self.filtrar, width=12, height=2)
+        btt2 = Button(frame2, text='Filter', command=self.filtrar, width=12, height=2)
         btt2.grid(row=0, column=5)
 
-        btt3 = Button(frame2, text='Actualizar', width=12, height=2)
+        btt3 = Button(frame2, text='Update',command=self.actualizar, width=12, height=2)
         btt3.grid(row=1, column=4)
 
-        btt4 = Button(frame2, text='Eliminar',command=self.eliminar, width=12, height=2)
+        btt4 = Button(frame2, text='Delete',command=self.eliminar, width=12, height=2)
         btt4.grid(row=1, column=5)
 
-        btt5 = Button(master=frame3,text="cerrar",bg="red",command=self.cerrar, width=10, height=3, font=15)
-        btt5.pack(side="bottom")
+        btt5 = Button(master=frame3,text="Close",bg="red",command=self.cerrar, width=10, height=3, font=15)
+        btt5.pack(side=BOTTOM)
 
-        self.drop_list = ttk.Combobox(frame3, values=["Calorias Cardio", "Peso", "Calorias quemadas Totales"], state="readonly")
-        self.drop_list.set("Selecciona la medida a graficar")
+        self.drop_list = ttk.Combobox(frame3, values=["Cardio Calories", "Weight", "Total Calories Burnt"], state="readonly")
+        self.drop_list.set("Select the measure to plot")
         self.drop_list.pack(side=TOP,expand=YES, fill="x")
 
-        btt6 = Button(master=frame3,text="Actualizar Grafico",command=self.actualizar_grafico, width=12, height=2)
+        btt6 = Button(master=frame3,text="Update Graphic",command=self.actualizar_grafico, width=12, height=2)
         btt6.pack(side=TOP,expand=YES, fill="x")
 
 
         self.ent7 = Entry(frame4)
-        self.ent7.place(x=10, y=415, width=460)
-        btt8 = Button(master=frame4,text="Insertar texto", command=self.chatbot_dialogue, width=15, height=2)
+        self.ent7.place(x=10, y=415, width=250)
+        btt8 = Button(master=frame4,text="Ask Chatbot", command=self.chatbot_dialogue, width=15, height=2)
         btt8.grid()
+        btt9 = Button(master=frame4,text="Clean Console", command=self.clean_console, width=15, height=2)
+        btt9.place(x=450, y=405)
 
         self.graficar()
 
@@ -166,9 +213,9 @@ class Pesos:
 
         fig, ax = plt.subplots()
         ax.plot(lista_y, lista_x, marker='o')
-        ax.set_title(f"Evolutivo Peso por fecha")
-        ax.set_xlabel("Fecha")
-        ax.set_ylabel(f"Peso")           
+        ax.set_title(f"Weight Evolution by Date")
+        ax.set_xlabel("Date")
+        ax.set_ylabel(f"Weight")           
 
         canvas = FigureCanvasTkAgg(fig, master=self.frame3)
         canvas.draw()
@@ -176,6 +223,9 @@ class Pesos:
         canvas.get_tk_widget().pack(fill="both", expand=True)
 
         self.canvas = canvas
+
+    def clean_console(self):
+        self.text.delete("1.0", END)  
 
     def actualizar_grafico(self):
         self.canvas.get_tk_widget().destroy()
@@ -192,15 +242,15 @@ class Pesos:
 
         for index in range(num_regis):
             y = datos[index][0] 
-            if self.get_selection().lower() == "peso":
+            if self.get_selection().lower() == "weight":
                 x = datos[index][1]
-            elif self.get_selection().lower() == "calorias cardio":
+            elif self.get_selection().lower() == "cardio calories":
                 x = datos[index][9]
-            elif self.get_selection().lower() == "calorias quemadas totales":
+            elif self.get_selection().lower() == "total calories burnt":
                 x = datos[index][10]
             else: 
                 #Imprimir un texto por pantalla para que el usuario vea que no eligio la medida a graficar
-                text = "Error en la eleccion de la medida a graficar, selecciona la medida y actualiza el grafico"
+                text = "There was an error in the measure chosen, select other measure and update the graph" 
                 self.label_text = ttk.Label(self.frame3, text=text, font=25, justify="center")
                 self.label_text.pack()
                 return ""
@@ -213,8 +263,8 @@ class Pesos:
         fig, ax = plt.subplots()
         ax.plot(lista_y, lista_x, marker='o')
 
-        ax.set_title(f"Evolutivo {self.drop_list.selection_get()} por fecha")
-        ax.set_xlabel("Fecha")
+        ax.set_title(f"{self.drop_list.selection_get()} Evolution by Date")
+        ax.set_xlabel("Date")
         ax.set_ylabel(f"{self.drop_list.selection_get().lower()}")       
 
         canvas = FigureCanvasTkAgg(fig, master=self.frame3)
@@ -232,8 +282,13 @@ class Pesos:
         input = self.ent7.get()
         print(input)
         self.ent7.delete(0, END)
-        response = model_LLM_response(input=input)
-        cleaned_response = response.split(input.split()[-1],1)[1].lstrip().capitalize()
+        response = get_response_LLM_pretrained(input_sentence=input)
+        print(response)
+        if "?" in response:
+            cleaned_response = response.split("?",1)[1].lstrip().capitalize()
+        else: 
+            cleaned_response = response.lstrip().capitalize()
+        #cleaned_response = response.split(input.split()[-1],1)[1].lstrip().capitalize()
         chatbot_response = str("Chatbot: ") + cleaned_response + "\n" 
 
         self.text.insert(INSERT, chatbot_response)
@@ -248,7 +303,7 @@ class Pesos:
         book = self.trv.get_children()
         for element in book:
             self.trv.delete(element)
-        query = 'SELECT * FROM control_pesos ORDER BY Fecha DESC'
+        query = 'SELECT * FROM weight_control ORDER BY Date DESC'
         rows = app.lanza_querys(query)
         for row in rows:
             self.trv.insert('', 0, text=row[1],values=row)
@@ -262,13 +317,13 @@ class Pesos:
             book = self.trv.get_children()
             for element in book:
                 self.trv.delete(element)
-            query = f'SELECT * FROM control_pesos WHERE Fecha > "{self.ent1.get()}" ORDER BY Fecha DESC'
+            query = f'SELECT * FROM weight_control WHERE Date > "{self.ent1.get()}" ORDER BY Date DESC'
             rows = app.lanza_querys(query)
             for row in rows:
                 self.trv.insert('', 0, text=row[1],values=row)
             self.ent1.delete(0,END)
         else: 
-            print('Filtro no aplicado')
+            print('The filter was not applied')
             self.consulta()
     
     def agregar(self):
@@ -285,15 +340,12 @@ class Pesos:
                 else:
                     fecha_reg_anterior = lectura_filas()[-1][0]
                     peso = float(self.ent2.get())
-                    peso_ayer = float(app.auxilia_consultas('Peso',fecha_reg_anterior)[0][0])
-                estatura = int(170)
-                MET_gym = float(6)
-                MET_cardio = float(3.5)
+                    peso_ayer = float(app.auxilia_consultas('Weight',fecha_reg_anterior)[0][0])
                 cardio = bool(self.ent4.get())
                 tiempo_entreno = int(self.ent3.get())
                 tiempo_cardio = int(self.ent5.get())
                 edad = int(self.ent6.get())
-                query = f"INSERT INTO control_pesos VALUES('{fecha}',{peso},{peso_ayer},{round(0, 2)}, {round(peso-peso_ayer,2)}, {round(0,2)}, {round(peso/(estatura/100),2)}, {round((((MET_gym*3.5*peso)/200)*tiempo_entreno),2)}, {cardio}, {round(tiempo_cardio*((MET_cardio*3.5*peso)/200),2)},{round((((MET_gym*3.5*peso)/200)*tiempo_entreno)+(tiempo_cardio*((MET_cardio*3.5*peso)/200))+((10*peso)+(6.25*estatura)-(5*(edad)+5)),2)})"
+                query = f"INSERT INTO weight_control VALUES('{fecha}',{peso},{peso_ayer},{round(0, 2)}, {round(peso-peso_ayer,2)}, {round(0,2)}, {round(peso/(self.estatura/100),2)}, {round((((self.MET_gym*3.5*peso)/200)*tiempo_entreno),2)}, {cardio}, {round(tiempo_cardio*((self.MET_cardio*3.5*peso)/200),2)},{round((((self.MET_gym*3.5*peso)/200)*tiempo_entreno)+(tiempo_cardio*((self.MET_cardio*3.5*peso)/200))+((10*peso)+(6.25*self.estatura)-(5*(edad)+5)),2)})"
                 app.lanza_querys(query)
                 self.ent1.delete(0,END)
                 self.ent2.delete(0,END)
@@ -303,34 +355,48 @@ class Pesos:
                 self.ent6.delete(0,END)
                 self.consulta()
             else:
-                print('No se ha guardado')
+                print('It was not saved')
                 self.consulta()
 
     def eliminar(self): #Me falta crear un error cuando el resgitro no este en tabla
         if len(self.ent1.get()) != 0:
             fecha_borrado = self.ent1.get()
             try:
-                query = f'DELETE FROM control_pesos WHERE Fecha = "{fecha_borrado}"'
+                query = f'DELETE FROM weight_control WHERE Date = "{fecha_borrado}"'
                 app.lanza_querys(query)
             except ValueError as error:
-                print('Este registro no esta en la tabla')
+                print('This record it is not on the table')
+            self.ent1.delete(0,END)
             self.consulta()
     
     def actualizar(self):
         if len(self.ent1.get()) != 0 and (len(self.ent2.get()) != 0 or len(self.ent3.get()) != 0 or len(self.ent4.get()) != 0 or len(self.ent5.get()) != 0 or len(self.ent6.get()) != 0):
-            query = f'UPDATE control_pesos SET ... WHERE Fecha = "{self.ent1.get()}"'
-        #Dandole vueltas a crear una query con los campos que esten rellenos con for o con listas
+            lista_nombre_columnas = app.nombre_columnas()
+            fecha_actualizar = self.ent1.get() 
+            peso_ayer = float(app.auxilia_consultas('Weight',fecha_actualizar)[0][0])
+            query = f'UPDATE weight_control SET {lista_nombre_columnas[0]} = "{self.ent1.get()}",{lista_nombre_columnas[1]} = {self.ent2.get()},{lista_nombre_columnas[2]} = {peso_ayer},{lista_nombre_columnas[3]} = {round(0,2)},{lista_nombre_columnas[4]} = {round(float(self.ent2.get()) - peso_ayer,2)},{lista_nombre_columnas[5]} = {round(0,2)},{lista_nombre_columnas[6]} = {round(float(self.ent2.get())/(float(self.estatura)/100),2)},{lista_nombre_columnas[7]} = {round((((self.MET_gym*3.5*float(self.ent2.get()))/200)*float(self.ent3.get())),2)},{lista_nombre_columnas[8]} = {int(self.ent4.get())},{lista_nombre_columnas[9]} = {round(float(self.ent5.get())*((self.MET_cardio*3.5*float(self.ent2.get()))/200),2)},{lista_nombre_columnas[10]} = {round((((self.MET_gym*3.5*float(self.ent2.get()))/200)*float(self.ent3.get()))+(float(self.ent5.get())*((self.MET_cardio*3.5*float(self.ent2.get()))/200))+((10*float(self.ent2.get()))+(6.25*self.estatura)-(5*(float(self.ent6.get()))+5)),2)} WHERE Fecha = "{self.ent1.get()}"'
+            query = query.replace("(","").replace(",)","")
 
-
-
+            try:
+                app.lanza_querys(query)
+            except ValueError as error:
+                print('This record it is not on the table')
+                self.consulta()
+            
+            self.ent1.delete(0,END)
+            self.ent2.delete(0,END)
+            self.ent3.delete(0,END)
+            self.ent4.delete(0,END)
+            self.ent5.delete(0,END)
+            self.ent6.delete(0,END)
+            self.consulta()
 
 
 if __name__ == '__main__':
-    if os.path.exists(TABLA) == False:
+    if os.path.exists(TABLE) == False:
         creacion_tabla()
-        print(f"Se genero la tabla de datos en tu carpeta actual --> {os.getcwd()}")
-    else:
-        pass 
+        print(f"The datatable was created on this path in you FileSystem --> {os.getcwd()}")
+
     root = Tk()
     pesos = Pesos(root)
 
